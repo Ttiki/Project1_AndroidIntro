1115689cf2c102c1425fd3b11b3d80870f127331
Ttiki <clem64.combier@gmail.com>
2022-03-23 15:01:29 +0100

    oui

--- app/src/main/java/com/ttiki/activity3_geoloc/MainActivity.java
+++ app/src/main/java/com/ttiki/activity3_geoloc/MainActivity.java
@@ -1,18 +1,13 @@
 package com.ttiki.activity3_geoloc;
 
-import android.Manifest;
-import android.content.pm.PackageManager;
-import android.location.Location;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.tasks.OnSuccessListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -31,30 +26,30 @@ public class MainActivity extends AppCompatActivity {
         lonLab = (TextView) findViewById(R.id.longLab);
         getLonLabBtn = (Button) findViewById(R.id.btnGetLatLong);
 
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
-        geoLoc.getLastLocation()
-                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
-                    @Override
-                    public void onSuccess(Location loc) {
-                        // De temps en temps la géoloc retournée peut être null.
-                        // On test ce cas ici
-                        if (loc != null) {
-                            latLab.setText("Error!");
-                            lonLab.setText("Error!");
-                        }
-                        latLab.setText(Double.toString((double) loc.getLatitude()));
-                        lonLab.setText(Double.toString((double) loc.getLongitude()));
-                    }
-                });
+//        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+//            // TODO: Consider calling
+//            //    ActivityCompat#requestPermissions
+//            // here to request the missing permissions, and then overriding
+//            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+//            //                                          int[] grantResults)
+//            // to handle the case where the user grants the permission. See the documentation
+//            // for ActivityCompat#requestPermissions for more details.
+//            return;
+//        }
+//        geoLoc.getLastLocation()
+//                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+//                    @Override
+//                    public void onSuccess(Location loc) {
+//                        // De temps en temps la géoloc retournée peut être null.
+//                        // On test ce cas ici
+//                        if (loc != null) {
+//                            latLab.setText("Error!");
+//                            lonLab.setText("Error!");
+//                        }
+//                        latLab.setText(Double.toString((double) loc.getLatitude()));
+//                        lonLab.setText(Double.toString((double) loc.getLongitude()));
+//                    }
+//                });
 
     }
 }
