43428faba912b714a349a1eb2c0cf09a5e18c4b2
Clément COMBIER <clem64.combier@gmail.com>
2022-03-23 08:15:47 +0100

    Activity 2 - Planets
    Planets - activity 2 scroll list

---
+++ src/main/java/com/uppa/monapp/Page3.java
@@ -0,0 +1,18 @@
+package com.uppa.monapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.uppa.monapp.databinding.ActivityPage3Binding;
+
+public class Page3 extends AppCompatActivity {
+    ActivityPage3Binding ui;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ui = ActivityPage3Binding.inflate(getLayoutInflater());
+        setContentView(ui.getRoot());
+    }
+}
